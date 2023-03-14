5cce4f04a497b1c8f8c939a1c7e4f51a7accb5c3
Valentina <balendi.art@gmail.com>
2023-03-14 15:18:45 +0100

    Buttons

--- Excerc4/ContentView.swift
+++ Excerc4/ContentView.swift
@@ -10,15 +10,65 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           Spacer()
+            HStack(spacing: 50) {
+                ZStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 85.0, height: 60.0)
+                    HStack{
+                        Image(systemName: "suit.club.fill")
+                            .foregroundColor(.white)
+                        Text("Club")
+                            .foregroundColor(Color.white)
+                    }
+                }
+                ZStack{
+                    RoundedRectangle(cornerRadius: 20)
+                        .frame(width: 110.0, height: 60.0)
+                    HStack{
+                        Image(systemName: "suit.spade.fill")
+                            .foregroundColor(.white)
+                        Text("Spade")
+                            .foregroundColor(Color.white)
+                    }
+                }
+            }
+            Spacer()
+            Text(" Select a Suit")
+                .font(.title)
+                .foregroundColor(Color.gray)
+            Spacer()
+            ZStack{
+                HStack(spacing: 50)  {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 20)
+                            .frame(width: 130.0, height: 60.0)
+                            .foregroundColor(Color.red)
+                        HStack{
+                            Image(systemName: "suit.spade.fill")
+                                .foregroundColor(.white)
+                            Text("Diamond")
+                                .foregroundColor(Color.white)
+                        }
+                    }
+                    ZStack{
+                     RoundedRectangle(cornerRadius: 20)
+                            .frame(width: 100.0, height: 60.0)
+                        .foregroundColor(Color.red)
+                        HStack{
+                            Image(systemName: "heart.fill")
+                                .foregroundColor(.white)
+                            Text("Heart")
+                                .foregroundColor(Color.white)
+                        }
+                }
+                }
+            }
+            Spacer()
         }
         .padding()
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
