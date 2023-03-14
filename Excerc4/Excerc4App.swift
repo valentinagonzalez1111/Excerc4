7a92197ccb4ea2f5aa5b0143221d759be39d3c7e
Valentina <balendi.art@gmail.com>
2023-03-14 13:49:09 +0100

    Initial Commit

---
+++ Excerc4/Excerc4App.swift
@@ -0,0 +1,17 @@
+//
+//  Excerc4App.swift
+//  Excerc4
+//
+//  Created by Apprenant 18 on 14/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Excerc4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
